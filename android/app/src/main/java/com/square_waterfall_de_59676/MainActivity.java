0661ec302fb09ff32acbae58d532e9302703591c
Crowdbotics <engineering@crowdbotics.com>
2023-01-28 02:59:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/square_waterfall_de_59676/MainActivity.java
@@ -0,0 +1,15 @@
+package com.square_waterfall_de_59676;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "square_waterfall_de_59676";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-waterfall-de-59676",
+  url: "https://square-waterfall-de-59676.botics.co",
+  api: "https://square-waterfall-de-59676.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
